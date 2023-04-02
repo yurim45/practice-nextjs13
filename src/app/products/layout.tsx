a0017bfd6a0e29c453aae03166eb7613c73c5d8c
yrkim <icho0405@gmail.com>
2023-04-02 14:15:09 +0900

    레이아웃 챌린지💡

--- src/app/products/layout.tsx
+++ src/app/products/layout.tsx
@@ -5,12 +5,10 @@ import styles from './layout.module.css';
 const ProductsLayout = ({ children }: { children: ReactNode }) => {
   return (
     <>
-      <nav>
-        <ul className={styles.list}>
-          <li>여성복</li>
-          <li>남성복</li>
-          <li>강아지옷</li>
-        </ul>
+      <nav className={styles.nav}>
+        <a href=''>여성옷</a>
+        <a href=''>남성옷</a>
+        <a href=''>여성옷</a>
       </nav>
       <section>{children}</section>
     </>
