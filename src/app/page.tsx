f5f7bd33e9011820f4b65733e274845a086d8727
yrkim <icho0405@gmail.com>
2023-05-01 19:07:47 +0900

    next image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import os from 'os'; // 노드 APIs
 
 import Counter from '@/components/Counter';
+import Image from 'next/image';
 
 export default function Home() {
   console.log('안녕! - 서버'); // 서버 컴포넌트로 동작하므로 브라우저가 아닌 터미널에서 확인 가능
@@ -11,6 +12,13 @@ export default function Home() {
     <>
       <h1>홈페이지!</h1>
       <Counter />
+      {/* 외부 이미지를 사용하려면 next.config에 등록하고 width, height를 지정해야 함 */}
+      <Image
+        alt='shop'
+        src='https://images.unsplash.com/photo-1441986300917-64674bd600d8'
+        width={400}
+        height={400}
+      />
     </>
   );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
 import { getProducts } from '@/service/products';
 import MeowArticle from '@/components/MeowArticle';
+import clothesImage from '../../../public/images/clothes.jpg';
 
 // SSG로 렌더링하면 빌드 시점에 생성된 HTML페이지가 전송되므로 데이터가 변경되어도 반영되지 않는다
 // 이때 ISR 렌더링 기법을 사용하면 변경되는 시점을 지정하여 렌더링 할 수 있다
@@ -17,6 +19,9 @@ export default async function ProductsPage() {
   return (
     <>
       <h2>제품 소개 페이지</h2>
+      {/* priority 중요한 이미지니까 가장 먼저 보여줘야해! */}
+      {/* local의 이미지는 별도 width와 height를 지정하지 않아도 됨 */}
+      <Image alt='clothes' src={clothesImage} priority />
       <ul>
         {products.map(({ id, name }) => {
           return (
